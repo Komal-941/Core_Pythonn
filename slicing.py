0bdfe8aacabfa9323a48118d20cbabc0d2d4cfac
Komal Shelar <941komal@gmail.com>
2025-02-06 18:41:47 +0530

    slicing.py

---
+++ slicing.py
@@ -0,0 +1,18 @@
+
+# slicing
+
+#### syntax : Variabel_name[start index number (included):ending index number(excluded)]
+
+t = ("welcome")
+
+print(len(t)-1)           #to know the trailing index number
+
+t[0:4]
+
+t[1:-3:1]              #to get a required values
+
+l=[10,20,[30,40,[50,60,"hello"]],80]
+l[2][2][1]
+
+l[2][2][-1]
+
